5bb2681c6b224193c0ebb10c8dc4e1a7d3bf9e7e
Andrey Elxnitis <elxnitisandrei@yandex.ru>
2020-05-15 19:25:28 +0300

    внесение финальных правок

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,26 +1,18 @@
 const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close-button");
-const sendButton = document.querySelector(".popup__button");
-const profileName = document.querySelector(".profile__title");
-const profileText = document.querySelector(".profile__subtitle");
+const form = document.querySelector(".popup__container");
+let profileName = document.querySelector(".profile__title");
+let profileText = document.querySelector(".profile__subtitle");
 let popupName = document.querySelector(".popup__input_name");
-let popupText = document.querySelector(".popup__inpur_text");
+let popupText = document.querySelector(".popup__input_text");
 
 //функция открытия popup
 
 function openPopup() { 
     popup.classList.add("popup__active");
-
-    function profile(e) {
-        e.preventDefault();
-        popupName = document.querySelector(".popup__input_name");
-        popupText = document.querySelector(".popup__input_text");
-        profileName.textContent = popupName.value;
-        profileText.textContent = popupText.value;
-    
-        closePopup();
-    }
+    popupName.value = profileName.textContent;
+    popupText.value = profileText.textContent;
 }
 //функция закрытия popup
 
@@ -30,26 +22,15 @@ function closePopup() {
 
 //функция изменения данных
 
+
 function profile(e) {
     e.preventDefault();
-    popupName = document.querySelector(".popup__input_name");
-    popupText = document.querySelector(".popup__input_text");
     profileName.textContent = popupName.value;
     profileText.textContent = popupText.value;
 
     closePopup();
-}
-
-//функция закрытия popup и сохранения данных кнопкой enter
-
-function closePopupEnter(e) {
-    if (e.key === "Enter") {
-        profile(e);
-    }
-}
+} 
 
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
-sendButton.addEventListener("submit", profile); 
-popupName.addEventListener("keypress", closePopupEnter);
-popupText.addEventListener("keypress", closePopupEnter);
+form.addEventListener("submit", profile); 
