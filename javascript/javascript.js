f1519e856609003624d19ccfb5b92282515651cd
Andrey Elxnitis <elxnitisandrei@yandex.ru>
2020-05-21 22:07:56 +0300

    продолжение разработки функциональности

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,13 +1,17 @@
-const editButton = document.querySelector(".profile__edit-button"); //кнопка открытия popup редактирования информации
-const addButton = document.querySelector(".profile__add-button"); //кнопка открытия popup редактиования карточек
-const popup = document.querySelector(".popup"); //popup
-const popupCard = document.querySelector(".popup-card"); //popup-card
+const editButton = document.querySelector(".profile__edit-button"); //кнопка открытия popup редактирования профиля
+const addButton = document.querySelector(".profile__add-button"); //кнопка открытия popup редактирования карточек
 const closeButton = document.querySelector(".popup__close-button"); //кнопка закрытия popup
-const form = document.querySelector(".popup__container");
-let profileName = document.querySelector(".profile__title");
-let profileText = document.querySelector(".profile__subtitle");
-let popupName = document.querySelector(".popup__input_name");
-let popupText = document.querySelector(".popup__input_text");
+const closeButtonCard = document.querySelector(".popup__close-button_card"); //кнопка закрытия popup__card
+const popup = document.querySelector(".popup"); //popup
+const popupCard = document.querySelector(".popup__card"); //popup__card
+const form = document.querySelector(".popup__container"); //форма профиля
+const formCard = document.querySelector(".form__card"); //форма карточек
+let profileName = document.querySelector(".profile__title"); //имя профиля
+let profileText = document.querySelector(".profile__subtitle"); //текст профиля
+let popupName = document.querySelector(".popup__input_name"); //поле имени профиля в popup
+let popupText = document.querySelector(".popup__input_text"); //поле текст профиля в popup
+let popupCardLink = document.querySelector(".popup__input_link"); //находим поле ссылки в popup__card
+let popupCardTitle = document.querySelector(".popup__input_title"); //находим поле названия в popup__card
 
 //массив карточек 
 const initialCards = [
@@ -40,30 +44,21 @@ const initialCards = [
 //контейнер для карточек 
 const elements = document.querySelector(".elements");
 
-//функция добавления карточек
+//функция добавления карточек из массива
 function addCard (card) {
-    let elementTemplate = document.querySelector("#element-template").content; //находим заготовку
-    let element = elementTemplate.cloneNode(true); //копируем заготовку
+    const elementTemplate = document.querySelector("#element-template").content; //находим заготовку
+    const element = elementTemplate.cloneNode(true); //копируем заготовку
 
-    let elementImage = element.querySelector(".element__image"); //находим поле image
-    let elementTitle = element.querySelector(".element__title"); //находим поле title
+    const elementImage = element.querySelector(".element__image"); //находим поле image
+    const elementTitle = element.querySelector(".element__title"); //находим поле title
 
     elementImage.src = card.link; //вставляем ссылку из массива
     elementTitle.textContent = card.name; //вставляем текст из массива
 
-    elements.append(element);
+    elements.prepend(element); //добовляем в контейнер для карточек
 }
 
-initialCards.forEach(addCard);
-/*
-let elementTemplate = document.querySelector("#element-template").content;
-let element = elementTemplate.cloneNode(true);
-
-element.querySelector(".element__image").src = initialCards{el.link}
-element.querySelector(".element__title").textContent = 
-
-elements.append(element); */
-
+initialCards.forEach(addCard); //проходим по всему массиву
 
 //функция открытия popup
 function openPopup() { 
@@ -72,12 +67,12 @@ function openPopup() {
     popupText.value = profileText.textContent;
 }
 
+
 //функция закрытия popup
 function closePopup() {
     popup.classList.remove("popup__active");
-    popupCard.classList.remove("popup__active");
 
-}
+} 
 
 //функция изменения данных popup
 function profile(e) {
@@ -93,7 +88,31 @@ function openPopupCard() {
     popupCard.classList.add("popup__active");
 }
 
+//функция закрытия popup__card
+function closePopupCard() {
+    popupCard.classList.remove("popup__active")
+}
+
+//функция добавления новой карточки
+function cards(e) {
+    e.preventDefault(); //отмена стандартного submit
+    const elementTemplate = document.querySelector("#element-template").content; //находим заготовку
+    const element = elementTemplate.cloneNode(true); //копируем заготовку
+
+    const elementImage = element.querySelector(".element__image"); //находим поле image
+    const elementTitle = element.querySelector(".element__title"); //находим поле title 
+
+    elementImage.src = popupCardLink.value; //вставляем ссылку из поле popup
+    elementTitle.textContent = popupCardTitle.value; //вставляем текст из поле popup
+
+    elements.prepend(element);
+
+    closePopupCard();
+} 
+
 editButton.addEventListener("click", openPopup);
 addButton.addEventListener("click", openPopupCard);
 closeButton.addEventListener("click", closePopup);
-form.addEventListener("submit", profile); 
+closeButtonCard.addEventListener("click", closePopupCard);
+form.addEventListener("submit", profile);
+formCard.addEventListener("submit", cards);
